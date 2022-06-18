eab9f383a77eadd2c167c7ff1978266778638bed
dev@KC <dev@kitconseil.fr>
2022-06-18 09:36:31 +0200

    test: set TDD flow

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 # future fixtures
 import pytest
-import server # to have its Club object mocked
+import server  # to have its Club object mocked
 from server import app
 
 
@@ -11,16 +11,11 @@ app.config.update(
 )
 
 
-@pytest.fixture
-def client():
-    # client fixture to provide instances of test client against our server app
-    with app.test_client() as client:
-        yield client
 
 
-@pytest.fixture
-def Clubs():
-    # data fixture to mock club secretaries 
+@pytest.fixture(scope='function')
+def clubs():
+    # data fixture to mock club secretaries
     clubs = [
         {"name": "Test Secretary 1", "email": "ts1@club1.asso", "points": "15"},
         {"name": "Test Secretary 2", "email": "ts2@club2.asso", "points": "0"},
@@ -30,6 +25,35 @@ def Clubs():
     ]
     return clubs
 
+
+@pytest.fixture(scope='function')
+def competitions():
+
+    # data fixture to mock competitions events
+    competitions = [
+        {"name": "Festival 1", "date": "2023-03-27 10:00:00", "numberOfPlaces": "25"},
+        {"name": "Festival 2", "date": "2000-03-27 10:00:00", "numberOfPlaces": "5"},
+        {"name": "Festival 3", "date": "2022-09-27 10:00:00", "numberOfPlaces": "20"},
+        {"name": "Festival 4", "date": "2023-03-27 10:00:00", "numberOfPlaces": "2"},
+    ]
+    return competitions
+
+
+# @pytest.fixture(scope='function')
+# def mock_clubs(mocker, Clubs):
+#     mocker.patch.object(server, "clubs", Clubs)
+
+
+# @pytest.fixture(scope='function')
+# def mock_clubs_competitions(mocker, Clubs, Competitions):
+#     mocker.patch.object(server, "clubs", Clubs)
+#     mocker.patch.object(server, "competitions", Competitions)
+
+
 @pytest.fixture
-def mock_clubs(mocker, Clubs):
-    mocker.patch.object(server, "clubs", Clubs)
+def client(mocker, clubs, competitions):
+    mocker.patch.object(server, "clubs", clubs)
+    mocker.patch.object(server, "competitions", competitions)
+    # client fixture to provide instances of test client against our server app
+    with server.app.test_client() as client:
+        yield client
--- tests/unit_tests/test_root.py
+++ tests/unit_tests/test_root.py
@@ -1,5 +1,5 @@
-
 from flask import current_app, Flask
+
 
 def test_url_root_is_available(client):
     """
@@ -23,7 +23,7 @@ def test_url_root_is_home_page(client):
     assert expected in response.data
 
 
-def test_known_email_is_shown_welcome(client, mock_clubs):
+def test_known_email_is_shown_welcome(client):
     """
     GIVEN a list of clubs being mocked
     WHEN a known user post to the showSummary url (POST)
@@ -36,7 +36,7 @@ def test_known_email_is_shown_welcome(client, mock_clubs):
     assert expected_value in response_data
 
 
-def test_unknown_email_is_not_shown_welcome(client, mock_clubs):
+def test_unknown_email_is_not_shown_welcome(client):
     """
     GIVEN a list of clubs being mocked
     WHEN an uknown user post to the showSummary url (POST)
@@ -44,7 +44,59 @@ def test_unknown_email_is_not_shown_welcome(client, mock_clubs):
     """
     email = "unknown@noclub.asso"
     expected_value = 401
-    response = client.post("/showSummary", data={"email": email}, follow_redirects=True)
-    response_data = response.data.decode()
-    print('expected error: ', response_data) # tracking the error
+    response = client.post("/showSummary", data={"email": email}, follow_redirects=False)
+    # response_data = response.data.decode()
+    # print("expected error: ", response_data)  # tracking the error
     assert response.status_code == expected_value
+
+
+def test_url_book_goes_happy_to_booking_page(client):
+    """
+    GIVEN club and competition exists
+    WHEN the '/book/<competition>/<club>' page is requested (GET)
+    THEN check that the page responded is the booking page
+    """
+
+    club_name = "Test Secretary 3"
+    competition_name = "Festival 3"
+    url = "/book/" + competition_name + "/" + club_name
+    response = client.get(url, data={"competition": competition_name, "club": club_name}, follow_redirects=False)
+    expected_status = 200
+    expected = b"Booking for"
+    assert response.status_code == expected_status
+    assert expected in response.data
+
+# def test_url_book_goes_sad_to_booking_page(client):
+#     """
+#     GIVEN club exist but no competition exists
+#     WHEN the '/book/<competition>/<club>' page is requested (GET)
+#     THEN an error is araised
+#     """
+
+#     club_name = "Test Secretary 3"
+#     competition_name = "Vestival 3"
+#     url = "/book/" + competition_name + "/" + club_name
+#     response = client.get(url, data={"competition": competition_name, "club": club_name}, follow_redirects=False)
+#     expected_status = 404
+#     assert response.status_code == expected_status
+
+
+
+def test_url_book_goes_happy_to_purchase_page(client):
+    """
+    GIVEN selected club, competition and # of place to book
+    WHEN the '/purchasePlaces' page is requested (POST)
+    THEN check that the page responded is the Purchase page
+    """
+
+    club_name = "Test Secretary 3"
+    competition_name = "Festival 3"
+    places_to_book = 5
+    url = "/purchasePlaces"
+    response = client.post(url,
+    data={"competition": competition_name, "club": club_name, "places": places_to_book},
+     follow_redirects=False)
+    expected_status = 200
+    # expected = b"Booking for"
+    assert response.status_code == expected_status
+    # assert expected in response.data
