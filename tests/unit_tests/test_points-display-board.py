6dea31b47587f076d5354e0ff937294e3452c35d
dev@KC <dev@kitconseil.fr>
2022-06-19 19:11:19 +0200

    test: set a Failing test for feature#7

---
+++ tests/unit_tests/test_points-display-board.py
@@ -0,0 +1,15 @@
+from flask import current_app, Flask, session
+
+import constants
+
+def test_url_display_board_is_available(client):
+    """
+    GIVEN  a secretary logs into the app
+    WHEN the '/displayBoard' page is requested (GET)
+    THEN check that the response is valid
+    """
+
+    response = client.get("/displayBoard")
+    assert response.status_code == 200
+
+
