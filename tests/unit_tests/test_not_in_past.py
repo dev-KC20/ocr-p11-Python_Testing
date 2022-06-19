3273cee7c59ae56452b4f65db547ef2eb4f6f1ac
dev@KC <dev@kitconseil.fr>
2022-06-19 16:05:08 +0200

    test: set the failing test for issue#5

---
+++ tests/unit_tests/test_not_in_past.py
@@ -0,0 +1,44 @@
+from flask import current_app, Flask, session
+
+import constants
+
+
+
+def test_book_not_in_past(client):
+    """
+    GIVEN A secretary wishes to book a number of places for a competition
+    WHEN they '/purchasePlaces' (POST) on a past competition
+    THEN check they can't  book a place
+    """
+    expected = constants.BOOKING_COMPLETED
+    club_name = "Test Secretary 1"
+    competition_name = "Festival 2" # past event
+    places_to_book = 1
+    url = "/purchasePlaces"
+    response = client.post(
+        url, data={"competition": competition_name, "club": club_name, "places": places_to_book}, follow_redirects=True
+    )
+    response_data = response.get_data(as_text=True)
+    print("purchase data: ", response_data)  # tracking the error
+    assert expected not in response_data
+
+
+def test_book_in_future(client):
+    """
+    GIVEN A secretary wishes to book a number of places for a competition
+    WHEN they '/purchasePlaces' (POST) on a competition to come
+    THEN check they can book a place
+    """
+    expected = constants.BOOKING_COMPLETED
+    club_name = "Test Secretary 1"
+    competition_name = "Festival 4" # future  event
+    places_to_book = 1
+    url = "/purchasePlaces"
+    response = client.post(
+        url, data={"competition": competition_name, "club": club_name, "places": places_to_book}, follow_redirects=True
+    )
+    response_data = response.get_data(as_text=True)
+    print("purchase data: ", response_data)  # tracking the error
+    assert expected in response_data
+
+
